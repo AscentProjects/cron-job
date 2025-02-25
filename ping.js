656325f4fc86cef41029902fbf1c99eba3dba1e1
Danilo Sousa <89808208+danilloubr@users.noreply.github.com>
2025-02-25 15:42:07 -0300

    Create ping.js
    Signed-off-by: Danilo Sousa <89808208+danilloubr@users.noreply.github.com>

---
+++ ping.js
@@ -0,0 +1,14 @@
+const axios = require("axios");
+const cron = require("node-cron");
+
+cron.schedule("*/15 * * * *", async () => {
+    try {
+        await axios.get("https://expert-tools.onrender.com");
+        await axios.get("https://expert-tools-api.onrender.com");
+        console.log("Pings enviados com sucesso!");
+    } catch (error) {
+        console.error("Erro ao fazer requisições:", error.message);
+    }
+});
+
+console.log("Cron job iniciado!");
